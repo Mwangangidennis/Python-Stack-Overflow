1f35a712cef656843d4063f111993f2c9bfd03ab
Dennis Mwangangi <37217389+Mwangangidennis@users.noreply.github.com>
2021-06-15 07:13:30 +0300

    Update looptxtline.py

--- looptxtline.py
+++ looptxtline.py
@@ -4,3 +4,37 @@ for line in file:
     print('Printing Line')
     i += 1
 file.close()
+
+
+# or
+# Python program to count the number of lines in a text file
+
+# Opening a file
+# import counter as counter
+
+file = open("Quiz1.dat", "r")
+Counter = 0
+
+# Reading from file
+Content = file.read()
+CoList = Content.split("\n")
+
+for i in CoList:
+    if i:
+        Counter += 1
+
+print("This is the number of lines in the file")
+print(Counter)
+
+file.close()
+# Counting End
+
+file = open("Quiz1.dat", "r")
+c = Counter
+a = 1
+reading = file.readlines()
+
+while a < c:
+    print(a)
+    print(reading[a])
+    a = a + 1
